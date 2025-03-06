a0ce83d9cc57d92df54b91cf9ada08dae44e7b27
icarus02 <maybeicarus02@gmail.com>
2025-03-06 15:54:44 -0500

    Update sniperscope.lua

--- client/sniperscope.lua
+++ client/sniperscope.lua
@@ -1,9 +1,1 @@
 
-Citizen.CreateThread(function()
-    while true do
-       if IsFirstPersonAimCamActive() then
-       DisplaySniperScopeThisFrame()
-        end
-    Citizen.Wait(0)
-    end
-end)
